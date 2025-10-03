c425c782c5aab7984b7d1c177dde51c8caf210f0
Rasel Rahman <rasel1716@cseku.ac.bd>
2025-10-03 17:55:38 +0600

    Update Login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -10,7 +10,7 @@ export default function Login({ handleLogin }) {
   const handleSubmit = async (e) => {
     e.preventDefault();
   
-    const response = await fetch("http://localhost:5000/login", {
+    const response = await fetch("https://akz-backend.onrender.com/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ username, password }),
